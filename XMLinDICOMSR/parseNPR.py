31bb4ee0927e4aed8cf2c457fb34a0bf5f484067
Helge Egil Seime Pettersen <helge.e.s.pettersen@gmail.com>
2023-10-05 09:16:47 +0200

    Added volven regionskoder

--- XMLinDICOMSR/parseNPR.py
+++ XMLinDICOMSR/parseNPR.py
@@ -7,110 +7,114 @@ from bs4 import BeautifulSoup
 import xml.dom.minidom
 import numpy as np
 
+
 class ListStructureAssertionException(Exception):
-	pass
+   pass
+
 
 class NPRDocument:
-	def __init__(self, path: str, encoding: str = "utf-8") -> None: # Reads them fine with utf-8, crashes with iso-8859-1 even if the source document is the latter...?
-		self.path = path
-		self.encoding = encoding
+   def __init__(self, path: str, encoding: str = "utf-8") -> None:  # Reads them fine with utf-8, crashes with iso-8859-1 even if the source document is the latter...?
+      self.path = path
+      self.encoding = encoding
 
-		self.xml_doc = pathlib.Path(self.path).read_text().encode(self.encoding)
-		self.npr = NPR.MsgHead.from_xml(self.xml_doc)
-		self.inst = self.npr.Document.RefDoc.Content.Melding.Institusjon[0]
+      self.xml_doc = pathlib.Path(self.path).read_text().encode(self.encoding)
+      self.npr = NPR.MsgHead.from_xml(self.xml_doc)
+      self.inst = self.npr.Document.RefDoc.Content.Melding.Institusjon[0]
 
-		self.ICD10 = Terminologies.ICD10()
-		self.NKPK = Terminologies.NKPK()
+      self.ICD10 = Terminologies.ICD10()
+      self.NKPK = Terminologies.NKPK()
+      self.Region = Terminologies.Region()
 
-	def getXML(self) -> str:
-		return str(self.npr.to_xml(skip_empty=True))
+   def getXML(self) -> str:
+      return str(self.npr.to_xml(skip_empty=True))
 
-	def getPatients(self) -> list:
-		return [obj.Pasient for obj in self.inst.Objektholder]
+   def getPatients(self) -> list:
+      return [obj.Pasient for obj in self.inst.Objektholder]
 
-	def getPatientNrs(self) -> list:
-		return [obj.Pasient.pasientNr for obj in self.inst.Objektholder]
+   def getPatientNrs(self) -> list:
+      return [obj.Pasient.pasientNr for obj in self.inst.Objektholder]
 
-	def getPatient(self, pasientNr: int):
-		for objekt in self.inst.Objektholder:
-			for medisinskStraling in objekt.medisinskStraling:
-				if not medisinskStraling.medisinskStralingID == pasientNr:
-					continue
+   def getPatient(self, pasientNr: int):
+      for objekt in self.inst.Objektholder:
+         for medisinskStraling in objekt.medisinskStraling:
+            if not medisinskStraling.medisinskStralingID == pasientNr:
+               continue
 
-				return medisinskStraling
+            return medisinskStraling
 
-	def getReferencedVolumes(self, pasientNr: int) -> list:
-		# TODO: Sjekk at pasientNr == attr i medisinsk stråling her i stedet for å bruke indeksert patID
+   def getReferencedVolumes(self, pasientNr: int) -> list:
+      # TODO: Sjekk at pasientNr == attr i medisinsk stråling her i stedet for å bruke indeksert patID
 
-		vols = None
-		for objekt in self.inst.Objektholder:
-			for medisinskStraling in objekt.medisinskStraling:
-				if not medisinskStraling.medisinskStralingID == pasientNr:
-					continue
+      vols = None
+      for objekt in self.inst.Objektholder:
+         for medisinskStraling in objekt.medisinskStraling:
+            if not medisinskStraling.medisinskStralingID == pasientNr:
+               continue
 
-				if isinstance(vols, list):
-					raise ListStructureAssertionException('medisinskStraling encountered more than one')
+            if isinstance(vols, list):
+               raise ListStructureAssertionException('medisinskStraling encountered more than one')
 
-				vols = medisinskStraling.referansevolum
+            vols = medisinskStraling.referansevolum
 
-		return {v.referansevolumID: v.referansevolumNavn for v in vols}
+      return {v.referansevolumID: v.referansevolumNavn for v in vols}
 
-	def getDoseFractions(self, pasientNr: int) -> dict:
-		doseDict = dict()
+   def getDoseFractions(self, pasientNr: int) -> dict:
+      doseDict = dict()
 
-		structureDict = self.getReferencedVolumes(pasientNr)
+      structureDict = self.getReferencedVolumes(pasientNr)
 
-		for structureName in structureDict.values():
-			doseDict[structureName] = {'plan': list(), 'gitt': list()}
+      for structureName in structureDict.values():
+         doseDict[structureName] = {'plan': list(), 'gitt': list()}
 
-		for objekt in self.inst.Objektholder:
-			if not objekt.pasientNr == pasientNr:
-				continue
+      for objekt in self.inst.Objektholder:
+         if not objekt.pasientNr == pasientNr:
+            continue
 
-			for medisinskStraling in objekt.medisinskStraling:
-				for behandlingsserie in medisinskStraling.behandlingsserie:
-					for apparatFremmote in behandlingsserie.ApparatFremmote:
-						for doseBidrag in apparatFremmote.doseBidrag:
-							structureName = structureDict[doseBidrag.referansevolumID]
-							doseDict[structureName]['plan'].append(float(doseBidrag.planDose))
-							doseDict[structureName]['gitt'].append(float(doseBidrag.gittDose))
+         for medisinskStraling in objekt.medisinskStraling:
+            for behandlingsserie in medisinskStraling.behandlingsserie:
+               for apparatFremmote in behandlingsserie.ApparatFremmote:
+                  for doseBidrag in apparatFremmote.doseBidrag:
+                     structureName = structureDict[doseBidrag.referansevolumID]
+                     doseDict[structureName]['plan'].append(float(doseBidrag.planDose))
+                     doseDict[structureName]['gitt'].append(float(doseBidrag.gittDose))
 
-		return doseDict
+      return doseDict
 
-	def getDoseTotal(self, pasientNr: int) -> dict:
-		doseFractions = self.getDoseFractions(pasientNr)
-		
-		for structure, data in doseFractions.items():
-			data['gitt'] = np.sum(data['gitt'])
-			data['plan'] = np.sum(data['plan'])
+   def getDoseTotal(self, pasientNr: int) -> dict:
+      doseFractions = self.getDoseFractions(pasientNr)
 
-		return doseFractions
+      for structure, data in doseFractions.items():
+         data['gitt'] = np.sum(data['gitt'])
+         data['plan'] = np.sum(data['plan'])
 
-	def getEpisodes(self, pasientNr: int) -> list:
-		# episode id: diagnosis, treatment code
-		episodes = list()
-		for obj in self.inst.Objektholder:
-			if not obj.pasientNr == pasientNr:
-				continue
+      return doseFractions
 
-			for episode in obj.episode:
-				episodes.append(episode)
+   def getEpisodes(self, pasientNr: int) -> list:
+      # episode id: diagnosis, treatment code
+      episodes = list()
+      for obj in self.inst.Objektholder:
+         if not obj.pasientNr == pasientNr:
+            continue
 
-		return episodes
+         for episode in obj.episode:
+            episodes.append(episode)
 
-	def getDiagnosis(self, pasientNr: int) -> dict:
-		diagnoser = set()
+      return episodes
 
-		episodes = self.getEpisodes(pasientNr)
-		for episode in episodes:
-			for tilstand in episode.tilstand:
-				for kode in tilstand.kode:
-					diagnoser.add(kode.kodeVerdi)
+   def getDiagnosis(self, pasientNr: int) -> dict:
+      diagnoser = set()
 
-		return {diag: self.ICD10.getICD10Definition(diag) for diag in diagnoser}
+      episodes = self.getEpisodes(pasientNr)
+      for episode in episodes:
+         for tilstand in episode.tilstand:
+            for kode in tilstand.kode:
+               diagnoser.add(kode.kodeVerdi)
 
-	def getProsedyrer(self, pasientNr: int) -> str:
-		pass
+      return {diag: self.ICD10.getICD10Definition(diag) for diag in diagnoser}
+
+   def getProsedyrer(self, pasientNr: int) -> str:
+      pass
+
 
 NPR = NPRDocument("../Data/XML/NPR-TstHelge5.xml")
 print(NPR.getDiagnosis())
